89d45c47646bf58da55ed7ea63e65f04d40964d4
Ali Berk Bostancı <55848455+berkbstnc@users.noreply.github.com>
2023-04-01 21:31:41 +0300

    Customer Panel düzenlendi, yeni mimari pathleri güncellendi

--- CSMS.Web/Controllers/CustomerPanelController.cs
+++ CSMS.Web/Controllers/CustomerPanelController.cs
@@ -1,18 +1,50 @@
-﻿using System;
+﻿using CSMS.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace CSMS.Web.Controllers
 {
+
+
     [Authorize(Roles = "Customer")]
     public class CustomerPanelController : Controller
     {
-        // GET: CustomerPanel
+        public CustomerPanelController()
+        {
+        }
+
+        public CustomerPanelController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
         public ActionResult Index()
         {
-            return View();
+            var currentUserId = User.Identity.GetUserId();
+            var uinfo = UserManager.Users.FirstOrDefault(x => x.Id == currentUserId);
+
+            if (uinfo != null)
+                return View(uinfo);
+            return View("Not Found");
         }
     }
 }
